6377b8a5561ed2d5c4a758f0441201c065b18349
Lynderly Donita <ldonita@su-g5.msk.21-school.ru>
2021-01-17 06:47:54 +0300

    added char printers

--- sandbox/ft_printf.h
+++ sandbox/ft_printf.h
@@ -1,2 +1,6 @@
 #include "tests.h"
+#include "libft.h"
 
+int				ft_putchar_fd(char c, int fd);
+int				ft_putstr_fd(char *s, int fd);//  TODO: rewrite so that it returns number of digits
+void			ft_putnbr_fd(int n, int fd); // TODO: rewrite so that it returns number of digits
